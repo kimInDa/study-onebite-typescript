0e69bc5864d6dc34ab7035e83633144b0f6a519a
kimInDa <pinktail2327@naver.com>
2024-05-08 22:37:37 +0900

    [01_03] TSX(TypeScript Execute)로 실행하기
    1. tsx 설치 : sudo npm i -g tsx
    2. tsx src/index.ts 로 실행 바로 가능(컴파일과 node 실행 한번에 해준다.)

--- section_01_start/src/index.js
+++
@@ -1,4 +0,0 @@
-console.log("Hello TypeScript");
-var a = 1;
-
-// 2. node src/index.js 로 생성된 자바스크립트 파일 실행
--- section_01_start/src/index.ts
+++ section_01_start/src/index.ts
@@ -2,3 +2,4 @@ console.log("Hello TypeScript");
 const a: number = 1;
 
 // 1. tsc src/index.ts 로 컴파일하여 index.js 생성
+// 3. tsx 설치하면 tsx src/index.ts 로 실행 바로 가능(컴파일과 node 실행 한번에 해준다.)
