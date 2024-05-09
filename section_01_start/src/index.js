4d343fce220e13787b82f26309d3df7b3bad4742
kimInDa <pinktail2327@naver.com>
2024-05-09 21:52:22 +0900

    [01_06] 컴파일러 옵션 설정하기 : module 옵션 (CommonJS ver.)
    - 타입스크립트는 ES 모듈 시스템과 유사하게 export, import 키워드를 사용한다.
    - 그리고 컴파일 결과를 (module 옵션을 통해) CommonJS와 ESM(ES 모듈 시스템) 중에 선택이 가능하다.

--- section_01_start/src/index.js
+++ section_01_start/src/index.js
@@ -1,5 +1,7 @@
-const func = () => console.log("Hello");
-// target 옵션
-// -> 컴파일 결과 생성되는 자바스크립트 코드의 버전을 설정한다.
-// -> 따라서 ES5로 설정 시 화살표 함수는 함수 표현식으로 변환된다.
-// -> ESNext : 자바스크립트 최신 버전으로 설정
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const hello_1 = require("./hello");
+(0, hello_1.hello)();
+// module 옵션
+// -> 타입스크립트는 ES 모듈 시스템과 유사하게 export, import 키워드를 사용한다.
+// -> 그리고 컴파일 결과를 (module 옵션을 통해) CommonJS와 ESM(ES 모듈 시스템) 중에 선택이 가능하다.
