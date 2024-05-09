72236c2a432688dcf0dcac6cf800a7e33ce16e48
kimInDa <pinktail2327@naver.com>
2024-05-09 21:57:27 +0900

    [01_06] 컴파일러 옵션 설정하기 : outDir 옵션
    - 컴파일 결과 파일이 생성되는 위치를 지정한다.

--- section_01_start/dist/index.js
+++ section_01_start/dist/index.js
@@ -1,6 +1,4 @@
 import { hello } from "./hello";
-
 hello();
-
 // outDir 옵션
 // -> 컴파일 결과 파일이 생성되는 위치를 지정한다.
--- section_01_start/src/index.js
+++
@@ -1,5 +0,0 @@
-import { hello } from "./hello";
-hello();
-// module 옵션
-// -> 타입스크립트는 ES 모듈 시스템과 유사하게 export, import 키워드를 사용한다.
-// -> 그리고 컴파일 결과를 (module 옵션을 통해) CommonJS와 ESM(ES 모듈 시스템) 중에 선택이 가능하다.
