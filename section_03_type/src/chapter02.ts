b42d70929e1941ce703a1f2b9f3987ab8be35302
kimInDa <pinktail2327@naver.com>
2024-05-28 18:20:54 +0900

    [03_02] Never 타입

--- section_03_type/src/chapter02.ts
+++ section_03_type/src/chapter02.ts
@@ -17,3 +17,28 @@ function unknownExam() {
   // let str: string = unknownVar;
   // let bool: boolean = unknownVar;
 }
+
+/**
+ * Never 타입
+ * 모든 타입의 서브 타입
+ * 즉, 모든 집합의 부분 집합 = (수학에선) 공집합
+ * 모든 타입으로 업 캐스팅할 수 있다.
+ * 다른 타입은 never 타입으로 다운캐스트할 수 없다. -> 따라서 never 타입은 어떤 값도 저장하지 않는 경우에 활용하면 좋다.
+ */
+
+function neverExam() {
+  // 공집합을 반환한다. = 아무것도 반환하지 않는다.
+  function neverFunc(): never {
+    while (true) {}
+  }
+
+  // 모든 타입으로 업 캐스팅할 수 있다.
+  let num: number = neverFunc();
+  let str: string = neverFunc();
+  let bool: boolean = neverFunc();
+
+  // 다른 타입은 never 타입으로 다운캐스팅할 수 없다.
+  // let never1: never = 10;
+  // let never2: never = "string";
+  // let never3: never = true;
+}
